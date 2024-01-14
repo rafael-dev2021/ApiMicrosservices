24b6a41030bf088fcd54c5ae8600d74502d640d5
Rafael Silva <ainbot6@gmail.com>
2024-01-14 15:52:38 -0300

    Adding fluent validation

---
+++ ApiMicrosservicesProduct/FluentValidation/CategoryDtoValidationLibrary/CategoryDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using ApiMicrosservicesProduct.Dtos;
+using FluentValidation;
+
+namespace ApiMicrosservicesProduct.FluentValidation.CategoryDtoValidationLibrary;
+
+public class CategoryDtoValidator : AbstractValidator<CategoryDto>
+{
+    public CategoryDtoValidator()
+    {
+        RuleFor(category => category.Name)
+            .NotEmpty().WithMessage("Category name is required")
+            .Length(3, 50).WithMessage("Name should have between 3 and 100 characters");
+
+        RuleFor(category => category.Image)
+            .NotEmpty().WithMessage("Image is required")
+            .MaximumLength(600).WithMessage("Image should have a maximum length of 600 characters");
+    }
+}
---
+++ ApiMicrosservicesProduct/FluentValidation/ProductDtoValidationLibrary/ProductDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using ApiMicrosservicesProduct.Dtos;
+using FluentValidation;
+
+namespace ApiMicrosservicesProduct.FluentValidation.ProductDtoValidationLibrary;
+
+public class ProductDtoValidator : AbstractValidator<ProductDto>
+{
+    public ProductDtoValidator()
+    {
+        RuleFor(product => product.Name)
+            .NotEmpty().WithMessage("Product name is required")
+            .Length(3, 100).WithMessage("Name should have between 3 and 100 characters");
+
+        RuleFor(product => product.Description)
+            .NotEmpty().WithMessage("Description is required")
+            .Length(10, 5000).WithMessage("Description should have between 10 and 5000 characters");
+
+        RuleForEach(product => product.Images)
+            .Must(image => image.Length <= 600)
+            .WithMessage("Each image should have a maximum length of 600 characters");
+
+        RuleFor(product => product.Price)
+            .NotEmpty().WithMessage("Price is required")
+            .InclusiveBetween(1, 999).WithMessage("Price should be between 1 and 999");
+
+        RuleFor(product => product.Stock)
+            .NotEmpty().WithMessage("Stock is required")
+            .InclusiveBetween(1, 99).WithMessage("Stock should be between 1 and 99");
+    }
+}
